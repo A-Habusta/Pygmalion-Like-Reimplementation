8a7cbfaff2dfe62fff2d8cce8b14e1d7e63418b9
Adrian Habusta <119956160+A-Habusta@users.noreply.github.com>
2023-09-26 02:01:46 +0200

    Add factorial example built with actions

--- SoftwareProject/Program.fs
+++ SoftwareProject/Program.fs
@@ -1,5 +1,7 @@
 ﻿module SoftwareProject.Main
 
+open SoftwareProject.Factorial
+
 let testFunction name funcA funcB start stop =
     let sequenceA = seq { for i in start..stop -> funcA i }
     let sequenceB = seq { for i in start..stop -> funcB i }
@@ -7,7 +9,6 @@ let testFunction name funcA funcB start stop =
     printfn ""
     printfn "Testing %s..." name
 
-    // Ask if this is lazily evaluated
     let firstMismatch = Seq.zip sequenceA sequenceB |> Seq.tryFind (fun (a, b) -> a <> b)
     match firstMismatch with
     | Some item -> printfn "Test failed: Mismatch at item %A" item
@@ -16,4 +17,6 @@ let testFunction name funcA funcB start stop =
 [<EntryPoint>]
 let main argv =
     ignore argv
+    testFunction "factorial" referenceFactorial iconFactorial 0 10
+
     0
