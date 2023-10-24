3b5213e2b673a664e6a0fdf15cf13abf3af85ec7
Adrian Habusta <119956160+A-Habusta@users.noreply.github.com>
2023-10-24 10:22:40 +0200

    Fix webpack config to point to renaed project

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path")
 
 module.exports = {
     mode: "development",
-    entry: "./src/App.fsproj",
+    entry: "./src/PygmalionReimplementation.fsproj",
     devtool: "eval-source-map",
 
     devServer: {
