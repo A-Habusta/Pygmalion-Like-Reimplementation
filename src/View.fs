4589299270c76c52dd816656155148e279d6b187
Adrian Habusta <119956160+A-Habusta@users.noreply.github.com>
2024-05-03 13:42:27 +0200

    Change sentence formulation

--- src/View.fs
+++ src/View.fs
@@ -482,7 +482,7 @@ let private renderIntialPopup dispatch =
         [ Html.h3 "Before you begin!"
           Html.p "This web application is the output program of my bachelors thesis, written at the Charles University Faculty of Mathematics and Science."
           Html.p "The goal of this thesis was not to create a fully fledged programming system. Rather, it was to explore how the original system might have worked."
-          Html.p "For a quick guide on how to use the system, or a more in-depth guide into the topic, you can read my thesis here:"
+          Html.p "For a quick guide on how to use the system, or  more in-depth information about the topic, you can read my thesis here:"
           Html.a [
               prop.href "https://a-habusta.github.io/bachelors-thesis/thesis.pdf"
               prop.text "Thesis"
